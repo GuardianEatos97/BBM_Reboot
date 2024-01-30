fde18ee056f96882dfa3f5e93bb0004ced6187e3
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-01-31 00:51:29 +0200

    ListView Added

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -6,25 +6,22 @@ namespace MauiApp1
     {
 
         public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-        int count = 0;
+        
 
         public MainPage()
         {
             InitializeComponent();
             Navigation.PushAsync(new UserCreationPage());
+            Navigation.PushAsync(new BBMPage());
             BindingContext = this;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-           /* count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);*/
+            
+            Navigation.PushAsync(new UserCreationPage());
+            Navigation.PushAsync(new BBMPage());
+           
         }
     }
 

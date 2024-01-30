4b1fce297a7539a372015ba87cf39f2eb6382a48
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-01-30 23:47:22 +0200

    Changes to main page and creation of new pages

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -4,26 +4,27 @@ namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
-        // Launcher.OpenAsync is provided by Essentials.
+
         public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
         int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            Navigation.PushAsync(new UserCreationPage());
             BindingContext = this;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+           /* count++;
 
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            SemanticScreenReader.Announce(CounterBtn.Text);*/
         }
     }
 

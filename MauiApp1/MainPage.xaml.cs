18cadce762c74722f27a59b7bcd46cfe5c8fccbd
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-01-30 13:59:04 +0200

    BBM Link updated and new home page in progress

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace MauiApp1
+﻿using System.Windows.Input;
+
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
+        // Launcher.OpenAsync is provided by Essentials.
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
         int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
